2ece30498087d9c1659e8756dac0c91b1aa84dd2
glm051517-ship-it <glm051517@gmail.com>
2026-06-21 17:44:59 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,34 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`0078000408020401001c000f05001202c102c201000405002800000064002800030500060000042a0000000400012704000800012508000c00011d0c001000012210001400012518001c0001241c002000012704001c00100500640000041e000004000000000000000000000005000a000004240000000400012a04000800012a08000c00012a0c001000012714001800012c1c002000012a06001c00010a006400f4016400000400000000000000000000000500000000021e0000000400011b08000c00011910001400011b18001c00011e1c002000011d08001c000e050046006603320000040a002d0000006400140001050002000002190004000800012c0c001000012c1400180002272918001c00012c`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
